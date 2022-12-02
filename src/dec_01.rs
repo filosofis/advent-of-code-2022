d7cfbb567f87868ef0afa54c672947df9583b729
Oscar <olundqvist89@gmail.com>
2022-12-02 17:57:31 +0100

    Refactoring

--- src/dec_01.rs
+++ src/dec_01.rs
@@ -3,28 +3,25 @@ use std::fs;
 pub fn run() {
     let data = fs::read_to_string("dec_01.txt").expect("unable to read file");
 
-   let mut big_sum_one = 0;
-   let mut big_sum_two = 0;
-   let mut big_sum_three = 0;
-   let mut sum = 0;
+    let (mut big_sum_one, mut big_sum_two, mut big_sum_three, mut sum) = (0, 0, 0, 0);
 
-   for line in data.lines(){
-    if !line.is_empty(){
-        sum += line.parse::<i32>().unwrap();
-    } else {
-        if sum > big_sum_one{
-            big_sum_three = big_sum_two;
-            big_sum_two = big_sum_one;
-            big_sum_one = sum;
-        }else if sum > big_sum_two{
-            big_sum_three = big_sum_two;
-            big_sum_two = sum
-        }else if sum > big_sum_three{
-            big_sum_three = sum
+    for line in data.lines() {
+        if !line.is_empty() {
+            sum += line.parse::<i32>().unwrap();
+        } else {
+            if sum > big_sum_one {
+                big_sum_three = big_sum_two;
+                big_sum_two = big_sum_one;
+                big_sum_one = sum;
+            } else if sum > big_sum_two {
+                big_sum_three = big_sum_two;
+                big_sum_two = sum
+            } else if sum > big_sum_three {
+                big_sum_three = sum
+            }
+            sum = 0;
         }
-        sum = 0;
     }
-   }
-   println!("Part1: {}", big_sum_one);
-   println!("Part2: {}", (big_sum_one + big_sum_two + big_sum_three));
+    println!("Part1: {}", big_sum_one);
+    println!("Part2: {}", (big_sum_one + big_sum_two + big_sum_three));
 }
--- src/dec_02.rs
+++ src/dec_02.rs
@@ -3,92 +3,91 @@ use std::fs;
 pub fn run() {
     let data = fs::read_to_string("dec_02.txt").expect("unable to read file");
 
-    part1(&data);
-    part2(&data);
+    println!("Part1: {}", part1(&data));
+    println!("Part2: {}", part2(&data));
+    //part2(&data);
 }
 
-fn part1(data: &String){
+fn part1(data: &String) -> i32 {
     let mut total_score = 0;
-    for line in data.lines(){
-
+    for line in data.lines() {
         let mut round = line.split_whitespace();
 
         let p1 = round.next();
         let p2 = round.next();
 
         let mut score = 0;
-        match p1{
+        match p1 {
             Some("A") => {
-                match p2{
+                match p2 {
                     Some("X") => score += 4, //+1 for X +3 for draw
                     Some("Y") => score += 8, //+2 for Y +6 for win
                     Some("Z") => score += 3, //+3 for Z +0 for loss
-                    _ => print!("Bad data")
+                    _ => print!("Bad data"),
                 }
             }
             Some("B") => {
-                match p2{
+                match p2 {
                     Some("X") => score += 1, //+1 for X +0 for loss
                     Some("Y") => score += 5, //+2 for Y +3 for draw
                     Some("Z") => score += 9, //+3 for Z +6 for win
-                    _ => print!("Bad data")
+                    _ => print!("Bad data"),
                 }
             }
             Some("C") => {
-                match p2{
+                match p2 {
                     Some("X") => score += 7, //+1 for X +6 for win
                     Some("Y") => score += 2, //+2 for Y +0 for loss
                     Some("Z") => score += 6, //+3 for Z +3 for draw
-                    _ => print!("Bad data")
+                    _ => print!("Bad data"),
                 }
             }
-            _ => print!("Bad data")
+            _ => print!("Bad data"),
         }
 
         total_score += score;
     }
-    println!("{}", total_score);
+    total_score
 }
 
-fn part2(data: &String){
+fn part2(data: &String) -> i32 {
     let mut total_score = 0;
-    for line in data.lines(){
-
+    for line in data.lines() {
         let mut round = line.split_whitespace();
 
         let p1 = round.next();
         let p2 = round.next();
 
         let mut score = 0;
-        match p1{
+        match p1 {
             Some("A") => {
-                match p2{
+                match p2 {
                     Some("X") => score += 3, //+3 lose via Scissors
                     Some("Y") => score += 4, //+4 draw via Rock
                     Some("Z") => score += 8, //+8 win via Paper
-                    _ => print!("Bad data")
+                    _ => print!("Bad data"),
                 }
             }
             Some("B") => {
-                match p2{
+                match p2 {
                     Some("X") => score += 1, //+1 lose via Rock
                     Some("Y") => score += 5, //+5 draw via Paper
                     Some("Z") => score += 9, //+9 win via Scissors
-                    _ => print!("Bad data")
+                    _ => print!("Bad data"),
                 }
             }
             Some("C") => {
-                match p2{
+                match p2 {
                     Some("X") => score += 2, //+2 lose via Paper
                     Some("Y") => score += 6, //+6 draw via Scissors
                     Some("Z") => score += 7, //+7 win via Rock
-                    _ => print!("Bad data")
+                    _ => print!("Bad data"),
                 }
             }
-            _ => print!("Bad data")
+            _ => print!("Bad data"),
         }
 
         total_score += score;
     }
-    println!("{}", total_score);
+    total_score
 }
